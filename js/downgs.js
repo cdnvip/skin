44ebca848c1f23553bf897c4c4c6338b65e27044
hao nan ren <78871156+cdnvip@users.noreply.github.com>
2021-04-04 19:41:22 +0800

    Update downgs.js

--- js/downgs.js
+++ js/downgs.js
@@ -1,9 +1,8 @@
- var gsurl2='';
- var gsurl4=''; 
- var gsurl3='';
+var fk="/plus/fankui.php";
+var tp="http://";
+var ps="https://";
+var moc=".com/";
 
-
-/*rjdownload(2)*/
 function js_gsdown(){
-document.writeln("<p class=\'sendErr-wrap\'>有问题？ <a rel=\'nofollow\'  href=\'#\' target=\'_blank\'>点击报错+投诉+提问</a></p>");
+document.writeln("<p class=\'sendErr-wrap\'>有问题？ <a rel=\'nofollow\'  href=\'javascript:;\' onclick=\'load(0)\'\'>点击报错+投诉+提问</a></p>");
 }
